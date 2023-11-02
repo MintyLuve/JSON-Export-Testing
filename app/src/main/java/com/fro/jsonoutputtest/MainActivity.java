32530e23982de910a4e39d55f6abc545b35e6d86
Sophia <111466302+MintyLuve@users.noreply.github.com>
2023-11-02 17:50:30 -0400

    testing android-github interface

--- app/src/main/java/com/fro/jsonoutputtest/MainActivity.java
+++ app/src/main/java/com/fro/jsonoutputtest/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         writer.write(content.toString().getBytes());
         writer.close();
     }
-
+    //JSON class
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
