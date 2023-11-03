dad51255953b704f7046860b5c8cd70143c570ca
Sophia <111466302+MintyLuve@users.noreply.github.com>
2023-11-03 14:45:57 -0400

    hehehe Clear inputs button added, comments added.

--- app/src/main/java/com/fro/jsonoutputtest/MainActivity.java
+++ app/src/main/java/com/fro/jsonoutputtest/MainActivity.java
@@ -30,17 +30,22 @@ import java.io.IOException;
 //Car export test
 public class MainActivity extends AppCompatActivity {
 
-    // defining variables
+    // defining edit text
     EditText data1;
-    String outputData1 = "Input";
     EditText data2;
-    String outputData2;
+    //defining buttons
     Button submitButton;
     Button yesButton;
     Button noButton;
-    TextView confirmation;
     Button nextPageButton;
+    Button clearButton;
+    //defining strings
+    String outputData1;
+    String outputData2;
+    TextView confirmation;
+    //defining preferences
     SharedPreferences myPrefs;
+
 
 
 
@@ -50,32 +55,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //initializing variables
-        submitButton = (Button) findViewById(R.id.submitButton);
+        //initializing edit text
         data1 = (EditText) findViewById(R.id.data1);
         data2 = (EditText) findViewById(R.id.data2);
+        // init buttons
+        submitButton = (Button) findViewById(R.id.submitButton);
         yesButton = (Button) findViewById(R.id.yesButton);
         noButton = (Button) findViewById(R.id.noButton);
-        confirmation = (TextView) findViewById(R.id.confirmation);
         nextPageButton = (Button) findViewById(R.id.nextPageButton);
+        clearButton = (Button) findViewById(R.id.clearTextButton);
+        //init textview
+        confirmation = (TextView) findViewById(R.id.confirmation);
+        //init preferences
         myPrefs = getSharedPreferences("prefID", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = myPrefs.edit();
 
+        // setting data to save when reopening the page
         outputData1 = myPrefs.getString("1",outputData1);
         data1.setText(outputData1);
         outputData2 = myPrefs.getString("2",outputData2);
-        data1.setText(outputData2);
+        data2.setText(outputData2);
 
 
-
+        // switches activities
         nextPageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            @Override public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
                 startActivity(intent);
-            }
-        });
+            } });
 
+        // Updates variable (output1) when the text is changed
         data1.addTextChangedListener(new TextWatcher() {
             @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
             @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
@@ -84,6 +93,8 @@ public class MainActivity extends AppCompatActivity {
                 editor.apply();
                 confirmation.setText(outputData1);
             } @Override public void afterTextChanged(Editable s) {} });
+
+        // Updates variable (output2) when the text is changed
         data2.addTextChangedListener(new TextWatcher() {
             @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
             @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
@@ -93,6 +104,27 @@ public class MainActivity extends AppCompatActivity {
                 confirmation.setText(outputData2);
             } @Override public void afterTextChanged(Editable s) {} });
 
+        //clears inputs
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputData1 = null;
+                data1.setText(outputData1);
+                editor.putString("1", outputData1);
+                editor.apply();
+
+                outputData2 = null;
+                data2.setText(outputData2);
+                editor.putString("1", outputData2);
+                editor.apply();
+                confirmation.setText("Send to Database?");
+                yesButton.setVisibility(View.INVISIBLE);
+                noButton.setVisibility(View.INVISIBLE);
+                confirmation.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        // when the submit button is clicked it makes options visible
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -103,6 +135,7 @@ public class MainActivity extends AppCompatActivity {
                     confirmation.setText("Send to Database?");
                 }
 
+                //If yes button is clicked, sets the buttons invisible, and outputs the data into a JSON
                 yesButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -122,6 +155,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+                //If the no button is clicked it hides the buttons
                 noButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -132,9 +166,9 @@ public class MainActivity extends AppCompatActivity {
                 });
 
 
-
             }
 
+            // sends data to a json
             public void toJSON(JSONObject content) throws IOException {
                 // Class to put the data into a JSON object
                 File path = getApplicationContext().getFilesDir();
